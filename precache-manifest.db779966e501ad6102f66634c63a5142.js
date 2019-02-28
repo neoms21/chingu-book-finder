c588f1ba03194722a9e7707edd2e82ce3b8be5ba
Manoj Sethi <msethi@qubittech.co.uk>
2019-02-28 23:04:01 +0000

    Updates

---
+++ precache-manifest.db779966e501ad6102f66634c63a5142.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c509247c4061f6ec96d",
+    "url": "/chingu-book-finder/static/css/main.6216c393.chunk.css"
+  },
+  {
+    "revision": "2c509247c4061f6ec96d",
+    "url": "/chingu-book-finder/static/js/main.2c509247.chunk.js"
+  },
+  {
+    "revision": "d8f03a688b07d56d6741",
+    "url": "/chingu-book-finder/static/js/runtime~main.d8f03a68.js"
+  },
+  {
+    "revision": "3ba0bec0c84c0a4d4bbb",
+    "url": "/chingu-book-finder/static/css/2.668d0b99.chunk.css"
+  },
+  {
+    "revision": "3ba0bec0c84c0a4d4bbb",
+    "url": "/chingu-book-finder/static/js/2.3ba0bec0.chunk.js"
+  },
+  {
+    "revision": "415fad05b94c9cc5c31549d7c632d2a6",
+    "url": "/chingu-book-finder/static/media/no-img.415fad05.jpg"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/chingu-book-finder/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/chingu-book-finder/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/chingu-book-finder/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/chingu-book-finder/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/chingu-book-finder/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/chingu-book-finder/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/chingu-book-finder/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/chingu-book-finder/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/chingu-book-finder/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/chingu-book-finder/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/chingu-book-finder/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/chingu-book-finder/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/chingu-book-finder/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/chingu-book-finder/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/chingu-book-finder/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/chingu-book-finder/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "b9d2215dc1d9e156bdcea50c61c145a2",
+    "url": "/chingu-book-finder/index.html"
+  }
+];
